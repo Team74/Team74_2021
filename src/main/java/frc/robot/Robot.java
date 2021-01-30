54bcd8d36b1e44d079bad472cdd8fc4da5288550
Martin <c@c.c>
2021-01-30 09:49:53 -0500

    commit test nameed dalfhaf

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -78,7 +78,10 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  // hello world
+
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
